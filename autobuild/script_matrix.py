60471a9efbc2cb40e201f71b29ab26d489bc032d
Richard <richardhayes777@gmail.com>
2022-10-14 10:16:36 +0100

    generalised script matrix script

--- autobuild/script_matrix.py
+++ autobuild/script_matrix.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
-
 import json
 import os
 import sys
 
 
-def make_script_matrix(project, directory):
-    _, directories, _ = list(os.walk(directory))[0]
+def make_script_matrix(directory, project):
+    _, directories, _ = list(os.walk(f"{project}/{directory}"))[0]
     directory_dicts = [
         {"project": project, "directory": directory}
         for directory in directories + ["."]
     ]
-    return json.dumps(directory_dicts)
+    return directory_dicts
+
+
+def make_combined_script_matrix(directory, *projects):
+    return [
+        item for project in projects for item in make_script_matrix(directory, project)
+    ]
 
 
 if __name__ == "__main__":
-    print(make_script_matrix(*sys.argv[1:]))
+    print(json.dumps(make_combined_script_matrix(*sys.argv[1:])))
